2c86fbbf09b6468d48ab4c9ade5b2b5ca3eef898
joeffrey_lamy_3006 <joeffreylamy@hotmail.com>
2018-01-30 17:31:17 +0000

    implements download file, file auto-deletion and get_filename

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'pry'
+require 'net/http'
 require './pdfier'
 
 get '/' do
@@ -7,6 +8,16 @@ get '/' do
 end
 
 post '/save_as_pdf' do 
-	Pdfier.save_as_pdf(params[:url], params[:filename])
-	redirect '/'
+	filename = Pdfier.new(params[:url], params[:filename]).save_as_pdf
+	redirect "/download/#{filename}"
+end
+
+get '/download/:filename' do |filename|
+	begin
+		send_file "./files/#{filename}", filename: filename, type: 'Application/octet-stream'
+	ensure
+		# FileUtils.remove_file("./files/#{filename}", force = true)
+		File.delete("./files/#{filename}")
+		redirect '/'
+	end
 end
--- pdfier.rb
+++ pdfier.rb
@@ -1,8 +1,18 @@
 class Pdfier
 
-	def self.save_as_pdf(url, filename)
-		`wkhtmltopdf #{url} #{filename}`
-		`open #{filename}`
+	def initialize(url, filename)
+		@url = url
+		@filename = filename
 	end
 
+	def save_as_pdf
+		`wkhtmltopdf #{@url} ./files/#{get_filename}`
+		get_filename
+	end
+
+	private
+	def get_filename
+		return @filename if @filename.match(/\.pdf$/)
+		"#{@filename}.pdf"
+	end
 end
